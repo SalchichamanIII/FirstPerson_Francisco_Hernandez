42c2c8f0e34bbd96471b5039684ce0c8a002925c
SalchichamanIII <163415869+SalchichamanIII@users.noreply.github.com>
2024-11-06 17:16:12 +0100

    61124

---
+++ Assets/Scripts/ArmaSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Arma")]
+public class ArmaSO : ScriptableObject
+{
+    //Datos
+    public int balasCargador;
+    public int balasBolsa;
+    public float cadenciaAtaque; 
+    public float distanciaAtaque;
+    public float danhoAtaque;
+
+}
