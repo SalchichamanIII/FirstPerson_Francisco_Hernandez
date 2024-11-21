6a359f85e20f3bb94d6732dddc838c2ba4acd31a
SalchichamanIII <163415869+SalchichamanIII@users.noreply.github.com>
2024-11-21 19:23:30 +0100

    211124

--- Assets/Scripts/Granada.cs
+++ Assets/Scripts/Granada.cs
@@ -6,7 +6,11 @@ public class Granada : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float fuerzaImpulso;
+    [SerializeField] private float radioExplosion;
     [SerializeField] private GameObject explosion;
+    [SerializeField] private LayerMask queEsExplotable;
+
+    private Collider[] buffer = new Collider[100];
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +24,30 @@ public class Granada : MonoBehaviour
     {
         transform.Rotate(new Vector3(1,1,1) * Random.Range(0,500) * Time.deltaTime);
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
 
     //Se ejecuta automaticamente cuando esta entidad se va a morir
     private void OnDestroy()
     {
         Instantiate(explosion, transform.position, Quaternion.identity);
+        //Si este if se cumple la granada ha detectado al menos un collider en la capa que es explotable
+        int numerosDetectados = Physics.OverlapSphereNonAlloc(transform.position, radioExplosion, buffer, queEsExplotable);
+        
+          if(numerosDetectados > 0)
+        {
+            //Recorrer todos los cols detectados
+            for(int i = 0; i < numerosDetectados; i++)
+            {
+                //Y por cada col detectado (HUESOS) voy a coger el script de cada uno
+                if(buffer[i].TryGetComponent(out ParteDeEnemigo scriptHueso))
+                {
+                    scriptHueso.Explotar();
+                }
+            }
+        }
+        
     }
 }
--- Assets/Scripts/ParteDeEnemigo.cs
+++ Assets/Scripts/ParteDeEnemigo.cs
@@ -26,4 +26,9 @@ public class ParteDeEnemigo : MonoBehaviour
             }
         }
     }
+
+    public void Explotar()
+    {
+
+    }
 }
